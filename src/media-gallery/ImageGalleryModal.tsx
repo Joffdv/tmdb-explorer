a64561d3ab3bb0a0769f549d59f9e1ec11257ec5
João Emanuel <107960713+Joffdv@users.noreply.github.com>
2023-01-02 17:40:24 -0300

    Add files via upload

---
+++ src/media-gallery/ImageGalleryModal.tsx
@@ -0,0 +1,54 @@
+import MediaGalleryModal from './MediaGalleryModal';
+import { Maybe } from '@/common/CommonTypes';
+import useApiConfiguration from '@/api-configuration/useApiConfiguration';
+import BaseImage from '@/common/BaseImage';
+import { IconButton, styled } from '@mui/material';
+import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+
+const FullScreenButton = styled(IconButton)(({ theme }) => ({
+  position: 'absolute',
+  top: theme.spacing(1),
+  right: theme.spacing(1),
+}));
+
+interface ImageGalleryModalProps {
+  title: string;
+  filePaths: Maybe<string[]>;
+}
+
+function ImageGalleryModal({ title, filePaths }: ImageGalleryModalProps) {
+  const { getImageUrl } = useApiConfiguration();
+
+  return (
+    <MediaGalleryModal
+      title={title}
+      dataSource={filePaths}
+      queryParamName="view"
+      renderMedia={({ mediaSrc, isFullScreen, toggleFullScreen }) => {
+        return (
+          <>
+            <BaseImage
+              src={getImageUrl(mediaSrc, {
+                quality: 'original',
+              })}
+              alt={title}
+              width={16}
+              height={9}
+              layout="responsive"
+              objectFit="contain"
+            />
+            <FullScreenButton
+              aria-label={isFullScreen ? 'Exit Fullscreen' : 'Enter Fullscreen'}
+              onClick={toggleFullScreen}
+            >
+              {isFullScreen ? <FullscreenExitIcon /> : <FullscreenIcon />}
+            </FullScreenButton>
+          </>
+        );
+      }}
+    />
+  );
+}
+
+export default ImageGalleryModal;
