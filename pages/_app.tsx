49e337f2570b12e8d7b1cfdf1d67d4778548df37
João Emanuel <107960713+Joffdv@users.noreply.github.com>
2023-01-03 11:14:15 +0000

    Finished movies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,80 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from 'react';
+import Head from 'next/head';
+import App, { AppContext, AppProps } from 'next/app';
+import AppLayout from '@/layout/AppLayout';
+import BaseDefaultSeo from '@/seo/BaseDefaultSeo';
+import { APP_TITLE } from '@/common/CommonConstants';
+import BaseThemeProvider, {
+  getInitialPaletteMode,
+} from '@/theme/BaseThemeProvider';
+import PageProgressBar from '@/common/PageProgressBar';
+import createEmotionCache from '@/theme/createEmotionCache';
+import { EmotionCache } from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import {
+  DehydratedState,
+  Hydrate,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { createQueryClient } from '@/http-client/queryClient';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PaletteMode } from '@mui/material';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+type PageProps = { dehydratedState: DehydratedState };
+
+type MyAppProps = AppProps<PageProps> & {
+  emotionCache?: EmotionCache;
+  initialPaletteMode: PaletteMode;
+};
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+  initialPaletteMode,
+}: MyAppProps) {
+  const [queryClient] = useState(() => createQueryClient());
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Head>
+            <title>{APP_TITLE}</title>
+            <meta
+              name="viewport"
+              content="initial-scale=1, width=device-width"
+            />
+            {process.env.GOOGLE_SITE_VERIFICATION && (
+              <meta
+                name="google-site-verification"
+                content={process.env.GOOGLE_SITE_VERIFICATION}
+              />
+            )}
+          </Head>
+          <BaseDefaultSeo />
+          <BaseThemeProvider initialPaletteMode={initialPaletteMode}>
+            <PageProgressBar />
+            <AppLayout>
+              <Component {...pageProps} />
+            </AppLayout>
+          </BaseThemeProvider>
+
+        </Hydrate>
+      </QueryClientProvider>
+    </CacheProvider>
+  );
 }
 
-export default MyApp
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  return {
+    ...appProps,
+    initialPaletteMode: getInitialPaletteMode(appContext.ctx),
+  };
+};
+
+export default MyApp;
